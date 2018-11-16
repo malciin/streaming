d4ac88264b8132903fe5f4d6577ae73e4f82add6
Marcin <malcinn@gmail.com>
2018-11-16 22:24:23 +0100

    Fix folder naming

---
+++ Streaming.Frontend/src/components/navbar/navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './navbar.css'
+import { NavLink } from "react-router-dom";
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="navbar-brand" href="#">
+                    <img src="pageLogo.png" alt="logo" />  
+                </div>
+                <div className="navbar">
+                    <ul className="navbar-nav">
+                        <li className="nav-item"><NavLink className="nav-link" to="/">Homepage</NavLink></li>
+                        <li className="nav-item"><NavLink className="nav-link" to="/Upload">Upload video</NavLink></li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default Navbar;
