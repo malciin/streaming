9a7783c9b24b8532ff2ec468c34ca7811d4cf56f
Marcin <malcinn@gmail.com>
2018-12-03 21:05:38 +0100

    Basic Play/Stop functionallity with ease controls animation

--- Streaming.Frontend/src/components/blocks/videoPlayer/videoPlayer.js
+++ Streaming.Frontend/src/components/blocks/videoPlayer/videoPlayer.js
@@ -7,13 +7,21 @@ export default class VideoPlayer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            manifestUrl: this.props.manifestUrl
-        }        
+            manifestUrl: this.props.manifestUrl,
+            videoPlayed: false,
+            hover: false
+        }
+        this.stopStartVideo = this.stopStartVideo.bind(this);
+        this.handleMouseOver = this.handleMouseOver.bind(this);
+
+        this.video = React.createRef();
     }
 
     componentDidMount() {
-        var video = document.getElementById('video');
-
+        var video = document.getElementById('video')
+        this.setState({
+            video: video
+        })
         if(Hls.isSupported()) {
             var hls = new Hls();
             hls.loadSource(this.state.manifestUrl);
@@ -21,10 +29,54 @@ export default class VideoPlayer extends React.Component {
             hls.on(Hls.Events.MANIFEST_PARSED,function() {
                 video.play();
             });
+            
+            this.setState({videoPlayed: true});
+        }
+    }
+
+    handleMouseOver() {
+        console.log("Before: " + this.state.hover);
+        this.setState({
+            hover: !this.state.hover
+        });
+        
+    }
+
+    stopStartVideo(event)
+    {
+        console.log(this.video);
+        if (this.video.current.paused)
+        {
+            this.video.current.play();
+            this.setState({videoPlayed: true});
+        }
+        else
+        {
+            this.video.current.pause();
+            this.setState({videoPlayed: false});
         }
     }
 
     render() {
-        return <video id="video"></video>
+        let icon = "";
+        if (this.state.videoPlayed)
+        {
+            icon = "icon-pause-outline";
+        }
+        else
+        {
+            icon = "icon-play-outline";
+        }
+        return <div id="video-wrapper" className="row" onMouseEnter={this.handleMouseOver}
+        onMouseLeave={this.handleMouseOver}>
+            <video 
+                id="video"
+                onClick={this.stopStartVideo} 
+                ref={this.video}
+                ></video>
+            <div className={ `video-control${this.state.hover ? " showed-controls" : ""}`} >
+                <i className={ icon + " play"} onClick={this.stopStartVideo}></i>
+            </div>   
+        </div>
     }
 }
