4a6339290d420c7cd62b632a9981e86f7ef25f62
Marcin Nowak <malcinn@gmail.com>
2019-03-05 21:25:38 +0100

    Startup cleanup

--- Streaming.Api/Startup.cs
+++ Streaming.Api/Startup.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Net;
-using System.Security.Cryptography;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using FluentValidation.AspNetCore;
@@ -10,8 +7,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Tokens;
-using Newtonsoft.Json.Linq;
 using Streaming.Api.Middlewares;
 using Streaming.Api.Monitor;
 using Streaming.Auth0;
@@ -20,29 +15,8 @@ using Streaming.Infrastructure.MongoDb.Extensions;
 
 namespace Streaming.Api
 {
-    
-
     public class Startup
     {
-        private SecurityKey GetAuth0RsaSecurityKey()
-        {
-            JObject jwks;
-            var jwksFileName = "jwks.json";
-            if (!File.Exists(jwksFileName))
-            {
-                using (var webClient = new WebClient())
-                {
-                    webClient.DownloadFile($"https://id0.eu.auth0.com/.well-known/jwks.json", jwksFileName);
-                }
-            }
-            jwks = JObject.Parse(jwksFileName);
-
-            var parameters = new RSAParameters();
-            parameters.Exponent = Base64UrlEncoder.DecodeBytes(jwks["e"].ToString());
-            parameters.Modulus = Base64UrlEncoder.DecodeBytes(jwks["n"].ToString());
-            return new RsaSecurityKey(parameters);
-        }
-
         private readonly IHostingEnvironment hostingEnvironment;
         private readonly IConfiguration configuration;
 
