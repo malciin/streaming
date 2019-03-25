ab2f29c5a3ecb4d3d3f803501c4292f6978cc301
Marcin Nowak <malcinn@gmail.com>
2019-03-25 23:20:33 +0100

    Fix permissions and container when its stopped and after that restarted. Additionally adding .gitattributes to force lf line ending in .sh files

--- Streaming.RTMPServer/makeRequest.sh
+++ Streaming.RTMPServer/makeRequest.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-echo `whoami` > /tmp/rec/test.txt
-echo `echo $UPLOAD_ENDPOINT` > /tmp/rec/test2.txt
-echo `printenv` > /tmp/rec/test3.txt
 curl -F "file=@$1" `echo $UPLOAD_ENDPOINT`
 rm $1
