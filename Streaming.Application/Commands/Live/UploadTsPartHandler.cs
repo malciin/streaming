ccc9994d9d592496623fab778c7057d4879b5395
Marcin Nowak <malcinn@gmail.com>
2019-03-29 23:11:54 +0100

    "Stable" streaming with ~30s delay. Need to work on frontend side

---
+++ Streaming.Application/Commands/Live/UploadTsPartHandler.cs
@@ -0,0 +1,21 @@
+﻿using Streaming.Application.Models;
+using System.Threading.Tasks;
+
+namespace Streaming.Application.Commands.Live
+{
+    public class UploadTsPartHandler : ICommandHandler<UploadTsPartCommand>
+    {
+        private readonly StreamManager streamManager;
+
+        public UploadTsPartHandler(StreamManager streamManager)
+        {
+            this.streamManager = streamManager;
+        }
+
+        public Task HandleAsync(UploadTsPartCommand Command)
+        {
+            streamManager.Upload(Command.StreamKey, Command.Part, Command.FileManifestDetails);
+            return Task.FromResult(0);
+        }
+    }
+}
