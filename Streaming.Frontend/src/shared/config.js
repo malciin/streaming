b2c0a13dacbffb00b2a4d1380e1e8fdc08d8f222
Marcin Nowak <malcinn@gmail.com>
2019-03-30 17:38:56 +0100

    add api env variables, update docker-compose

--- Streaming.Frontend/src/shared/config.js
+++ Streaming.Frontend/src/shared/config.js
@@ -1,5 +1,5 @@
 export const Config = {
-    apiPath: "http://localhost:8086",
+    apiPath: process.env.API_URL,
     auth0: {
         clientID: "kkF9cfb31YAOvr8R5q1FQ8byH9YiQXyr",
         domain: "id0.eu.auth0.com",
